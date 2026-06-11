cd201aa37234720aa911108bab903faff99502dc
akashk-byte <akashkumar84105@gmail.com>
2026-06-11 10:43:10 +0530

    Time: 1 ms (99.95%) | Memory: 77.3 MB (31.52%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,12 +1,10 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-       int cs= nums[0];
         int ms=nums[0];
+        int cs=nums[0];
         for(int i=1;i<nums.length;i++){
-             cs = Math.max(nums[i],cs+nums[i]);
-            //ms = Math.Max(ms,cs);
-             
-            ms= Math.max(ms,cs);
+            cs=Math.max(nums[i],cs+nums[i]);
+            ms=Math.max(ms,cs);
         }
         return ms;
     }
