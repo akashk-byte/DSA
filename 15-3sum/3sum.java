6cfed18459559258bf8cf60fe848929c07018404
akashk-byte <akashkumar84105@gmail.com>
2026-08-18 17:45:53 +0530

    Time: 35 ms (43.11%) | Memory: 60.3 MB (9.65%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,40 +1,39 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-       int n = nums.length;
-       Arrays.sort(nums);
-       List<List<Integer>> ans = new ArrayList<>();
-       //HashSet<List<Integer>> set = new HashSet<>();
-       for(int i=0;i<n-2;i++){
-        if(i>0 && nums[i]==nums[i-1]){
-            continue;
-        }
-        int left = i+1;
-        int right = n-1;
-        while(left<right){
-            int sum = nums[i]+nums[right]+nums[left];
-            if( sum == 0){
-                List<Integer> list = new ArrayList<>();
-                list.add(nums[right]);
-                list.add(nums[left]);
-                list.add(nums[i]);
-                ans.add(list);
-                left++;
-                right--;
-                while(left<right && nums[left]==nums[left-1]){
+        Arrays.sort(nums);
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=0;i<n-2;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            int right = n-1;
+            int left = i+1;
+            while(left<right){
+                int sum = nums[i]+nums[right]+nums[left];
+                if(sum==0){
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[right]);
+                    list.add(nums[left]);
+                    ans.add(list);
                     left++;
-                }
-                while(left<right && nums[right]==nums[right+1]){
                     right--;
+                    while(left<right && nums[left]==nums[left-1]){
+                        left++;
+                    }
+                    while(left<right && nums[right]==nums[right+1]){
+                        right--;
+                    }
                 }
-            }
-            else if(sum>0){
-                right--;
-            }
-            else{
-                left++;
+                    else if(sum>0){
+                        right--;
+                    }
+                    else{
+                        left++;
+                    }
             }
         }
-       }
-       return ans;
+            return ans;
     }
 }
