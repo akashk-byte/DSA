8dea916eaa73a2944a6af1f615cf98e9d205c391
akashk-byte <akashkumar84105@gmail.com>
2026-09-22 11:48:39 +0530

    Time: 7 ms (89.33%) | Memory: 45.7 MB (97.33%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -3,13 +3,14 @@ class Solution {
         int left = 0;
         int max= 0;
         int maxlength = 0;
-        HashMap<Character,Integer> map = new HashMap<>();
+        int[] freq = new int[256];
         for(int right=0;right<s.length();right++){
-            map.put(s.charAt(right),map.getOrDefault(s.charAt(right),0)+1);
-            max =  Math.max(max,map.get(s.charAt(right)));
+            int index  = s.charAt(right)-'A';
+            freq[index]++;
+            max = Math.max(max,freq[index]);
             while((right-left+1)-max>k){
-               map.put(s.charAt(left),map.get(s.charAt(left))-1);
-               left++;
+                freq[s.charAt(left)-'A']--;
+                left++;
             }
             int len = right-left+1;
             maxlength = Math.max(maxlength,len);
