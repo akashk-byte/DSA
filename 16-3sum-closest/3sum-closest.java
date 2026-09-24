96d7dc2b8dc5670cdd1dcf6d50a06a8df4165372
akashk-byte <akashkumar84105@gmail.com>
2026-09-24 11:59:00 +0530

    Time: 16 ms (92.85%) | Memory: 45.4 MB (66.72%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -3,28 +3,25 @@ class Solution {
         Arrays.sort(nums);
         int n = nums.length;
         int max_diff = Integer.MAX_VALUE;
-        int closest=nums[0]+nums[1]+nums[2];
-        for(int i=0;i<n-2;i++){
-            int left =i+1;
-            int right =nums.length-1;
+        int closest= 0;
+        for(int i=0;i<n;i++){
+            int left = i+1;
+            int right = n-1;
             while(left<right){
                 int sum = nums[i]+nums[left]+nums[right];
                 int diff = Math.abs(sum-target);
                 if(diff<max_diff){
-                    max_diff=diff;
-                    closest=sum;
+                    max_diff = diff;
+                    closest = sum;
                 }
                 else if(sum>target){
                     right--;
                 }
-                else if (sum<target){
+                else{
                     left++;
                 }
-                else{
-                    return target;
-                }
-                }
             }
-        return closest ;
+        }
+        return closest;
     }
 }
