11f94fc7f34f22e023c39691ebaaea59463be507
akashk-byte <akashkumar84105@gmail.com>
2026-09-25 11:56:32 +0530

    Time: 33 ms (69.96%) | Memory: 60 MB (14.86%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,43 +1,39 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
-        int n = nums.length;
-        for(int i =0;i<n-2;i++){
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i=0;i<nums.length-2;i++){
+            int j =i+1;
+        int k = nums.length-1;
             if(i>0 && nums[i]==nums[i-1]){
                 continue;
             }
-        //for(int i =0;i<n-2;i++){
-
-            int left = i+1;
-            int right = n-1;
-            
-            while(left<right){
-                int sum = nums[i]+nums[left]+nums[right];
-            if(sum==0){
-                List<Integer> temp = new ArrayList<>();
-                temp.add(nums[i]);
-                temp.add(nums[left]);
-                temp.add(nums[right]);
-                ans.add(temp);
-                left++;
-                right--;
-            
-            while(left<right && nums[left]==nums[left-1]){
-                left++;
+                
+                while(j<k){
+                    int sum = nums[i]+nums[j]+nums[k];
+                    if(sum == 0){
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        list.add(temp);
+                        j++;
+                        k--;
+                        while(j<k && nums[j]==nums[j-1]){
+                            j++;
+                        }
+                        while(j<k && nums[k]==nums[k+1]){
+                            k--;
+                        }
+                    }
+                    else if(sum>0){
+                        k--;
+                    }
+                    else{
+                        j++;
+                    }
+                }
             }
-            while(left<right && nums[right]==nums[right+1]){
-                right--;
-            }
-            }
-            else if(sum>0){
-                right--;
-            }
-            else{
-                left++;
-            }
-            }
-        }
-        return ans;
+        return list;
     }
 }
